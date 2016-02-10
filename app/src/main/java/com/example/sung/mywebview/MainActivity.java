ba8d127a04337a89f78b94e4893d9295b451ae4c
sung <sccmm2@naver.com>
2016-02-10 14:59:31 +0900

    second commit

--- app/src/main/java/com/example/sung/mywebview/MainActivity.java
+++ app/src/main/java/com/example/sung/mywebview/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.sung.mywebview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+    }
+
+    public void onButton1Clicked(View v) {
+        //webView.loadUrl("http://m.naver.com");
+        webView.loadUrl("file:///android_asset/sample.html");
     }
 }
